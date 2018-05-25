88b3c4f3b9dfe0fa93ad3eac7b134e43b3b96cc3
Chris Karayannidis <33937855+ChrisKarayann@users.noreply.github.com>
2018-05-25 16:44:39 +0300

    4 lib update
    moved functions to lib.js

--- mqtt2mongodb/mqtt2mongoDB.js
+++ mqtt2mongodb/mqtt2mongoDB.js
@@ -4,6 +4,7 @@
 var fs = require('fs');
 var ssu = require('./config.json');
 var crypto = require('crypto'); //ONLY WITH AUTHORIZATION
+var alib = require('./lib.js');
 var mqtt = require('mqtt');
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
@@ -12,74 +13,8 @@ var machineId, messageObj;
 
 //------------------ FIRST OF ALL HANDLE SECURITY ---------------------//
 //------------- REMOVE WHOLE BLOCK FOR NO AUTHORIZATION ---------------//
-function collectDeciph()
-{
-    //--------- CHECK IF AUTHORIZATION KEYS EXIST ---------//
-     function kfileExistence()
-     {
-         try
-         {
-            fs.accessSync(ssu.DGL.p + 'd/meta.json', fs.F_OK);
-            var keysFound = true;
-         }
-         catch(e)
-         {
-            keysFound = false;
-         }
-         return keysFound;
-      }
-      //---------------------------------------------------//
-
-      var kf = kfileExistence();
-
-      //--------- IF KEYS EXIST START DECIPHERING ---------//
-      function startDeciph()
-      {
-         if (kf == true)
-         {
-            var poa = require (ssu.DGL.p + 'd/meta.json')
-
-            const chain0 = crypto.createDecipheriv('aes-128-cbc',
-                                                    poa.asegk.ky,
-                                                    poa.asegk.iv);
-            let mqu = chain0.update(ssu.MQTT.user, 'hex', 'utf-8');
-            mqu += chain0.final('utf-8');
-
-            const chain1 = crypto.createDecipheriv('aes-128-cbc',
-                                                   poa.asegk.ky,
-                                                   poa.asegk.iv);
-            let mqp = chain1.update(ssu.MQTT.pass, 'hex', 'utf-8');
-            mqp += chain1.final('utf-8');
-
-            const chain2 = crypto.createDecipheriv('aes-128-cbc',
-                                                    poa.asegk.ky,
-                                                    poa.asegk.iv);
-            let mgu = chain2.update(ssu.MONGO.user, 'hex', 'utf-8');
-            mgu += chain2.final('utf-8');
-
-            const chain3 = crypto.createDecipheriv('aes-128-cbc',
-                                                   poa.asegk.ky,
-                                                   poa.asegk.iv);
-            let mgp = chain3.update(ssu.MONGO.pass, 'hex', 'utf-8');
-            mgp += chain3.final('utf-8');
-
-            return [mqu, mqp, mgu, mgp]; // RETURN VALID VALUES
-         }
-         else
-         {
-             //console.log("Authorization file not found!");//FOR DEBUG
-             return ["0", "0", "0", "0"]; // RETURN ZERO IF FILE NOT FOUND
-         }
-      };
-      //------------------------------------------------------//
-
-      var data = startDeciph();
-      return data;
-}
-
-var collected = collectDeciph(); // KEYS COLLECTED - DRIVE MAY BE REMOVED - NO KEYS PRESENT ON THE SYSTEM
-
-//---------------------------------------------------------------------//
+alib.mAuth();
+var collected = alib.mAuth(); // KEYS COLLECTED - DRIVE MAY BE REMOVED - NO KEYS PRESENT ON THE SYSTEM
 //--------------------- END OF AUTHORIZATION BLOCK --------------------//
 
 if (collected[0] != 0 &&  // REMOVE IF CONDITION IF NO AUTH
@@ -139,14 +74,7 @@ MongoClient.connect(mongoUrl, function(error, database) // CONNECT TO MONGO
               function(err, collection)
               {
                  if (err) throw err;
-                 //console.log("Insert doc in freshly created object collection. Check assign log");// FOR DEBUG
-                 fs.appendFile('assign.log', "New object assigned with ID: " +
-                                              objectID                       +
-                                              " -- "                         +
-                                              "when:" + new Date() + "\n", function(err)
-                                                                           {
-                                                                              if (err) throw err;
-                                                                           })
+                 alib.newAsgn();
                  collection.insert(cleanMessage, function(err, result)
                  {
                     if (err) throw err;
@@ -168,15 +96,7 @@ MongoClient.connect(mongoUrl, function(error, database) // CONNECT TO MONGO
       }
       catch(e) // POSSIBLE PARSE ERROR IS CATCHED HERE AND LOGGED TO A FILE
       {
-         //console.log("Bad Karma. Check error log"); // FOR DEBUG
-         fs.appendFile('error.log', "|---- BAD KARMA ----|\n" +
-                                        e     +    "\n"       +
-                                    "WHEN : " + new Date()    +
-                                             "\n"             +
-                                    "\n" + "\n" , function (err)
-                                                  {
-                                                     if (err) throw err;
-                                                  })
+        alib.prsErr(); 
       }
     });
   });
@@ -186,12 +106,6 @@ MongoClient.connect(mongoUrl, function(error, database) // CONNECT TO MONGO
 else
 
 {
-  //console.log("Authorization System Failure. Check error log");// FOR DEBUG
-  fs.appendFile('error.log', "|--- Authorization System Failure ---|" +
-                             "\n" + "WHEN : " + new Date() + "\n" +"\n"
-                             , function (err)
-                               {
-                                  if (err) throw err;
-                               })
+  alib.authErr();
 }
 //------------------------------------------------------------------//
